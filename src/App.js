252cdae6401c6c0429fb9f4e54cabb0687d7c51f
karim gharbi <58739019+Mirak005@users.noreply.github.com>
2020-08-07 11:57:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Profile from "./Profile/Profile";
+import userImg from "./images/user.jpg";
+import userImg2 from "./images/user2.jpg";
+import "./styles.css";
+
+export default function App() {
+  function handleName(name) {
+    alert(`Hello my name is ${name}`);
+  }
+
+  return (
+    <div className="App">
+      <Profile
+        fullName="John Doe"
+        bio="Hello , my name is John Doe and i like web development"
+        profession="Full-Stack js Developer"
+        handleName={handleName}
+      >
+        <img
+          style={{ borderRadius: "100%", width: "200px", height: "200px" }}
+          src={userImg}
+          alt="avatar"
+        />
+      </Profile>
+      <Profile
+        fullName="Jane Doe"
+        bio="Hello , my name is Jane Doe and i like music , i write songs , teach and play the piano"
+        profession="Music Teacher"
+        handleName={handleName}
+      >
+        <img
+          style={{ borderRadius: "100%", width: "200px", height: "200px" }}
+          src={userImg2}
+          alt="avatar"
+        />
+      </Profile>
+    </div>
+  );
+}
